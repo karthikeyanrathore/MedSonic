77e70f8714e806529b3504793622527f20c8197e
karthikeyanrathore <karthikerathore@gmail.com>
2021-11-15 16:49:39 +0530

    stupid me

--- build/pneumonia.py
+++ build/pneumonia.py
@@ -1,30 +1,29 @@
 #!/usr/bin/env python3
 class Model:
   def get():
-    import tensorflow as tf
-    # load json and create model
-    f = open('model/model.json', 'r')
-    savemodel= f.read()
-    model= tf.keras.models.model_from_json(savemodel)
-    model.summary()
-
-    # load weights
     from keras.models import load_model
-    model.load_weights('model/model.h5')
-    model.compile(loss='sparse_categorical_crossentropy', optimizer='SGD', metrics=['accuracy'])
+    # load json and create model
+    model = load_model("model/pneumonia_model.h5")
+    model.summary()
     return model
 
   def resize(path):
     import  skimage 
     from skimage import filters
     import cv2
+    import numpy as np
     img = cv2.imread(path)
-    return skimage.transform.resize(img, (200, 200, 3))
+    img = skimage.transform.resize(img, (200, 200, 3))
+    return np.expand_dims(img, axis=0)
     
   def predict(path):
     model = Model.get()
     img = Model.resize(path)
-    return model.predict(img)
+    result = model.predict(img)
+    if result[[0]] > 0.5:
+      return 1
+    else:
+      return 0
+
    
 
-x = Model.predict("data/person1946_bacteria_4874.jpeg")
