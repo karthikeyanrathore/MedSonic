20903f12ac6b7197ff5bb3c3daf345bf23dcb8e8
karthikeyan <karthikerathore@gmail.com>
2023-03-27 01:45:08 +0530

    saving web:

--- web/web/src/main.js
+++ web/web/src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import './assets/main.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import bootstrap from 'bootstrap/dist/js/bootstrap.js'
+
+createApp(App).use(bootstrap).mount('#app')
