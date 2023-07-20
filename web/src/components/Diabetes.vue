fe6f957cc284ec337da8ccf52a0851eb243e1407
karthikeyan <karthikerathore@gmail.com>
2023-07-20 20:37:51 +0530

    rm vue

--- web/src/components/Diabetes.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <figure class="text-center">
-        <h4>Diabetes NotImplemented.</h4>
-    </figure>
-</template>
-  
-<script>
-export default {}
-</script>
--- web/src/components/Pneumonia.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <figure class="text-center">
-        <h4>Pneumonia NotImplemented.</h4>
-    </figure>
-</template>
-  
-<script>
-export default {}
-</script>
